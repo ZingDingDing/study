1d8aef0d9382b77803630a0eeb31a28d79cc282c
ZingDingDing <912249503@qq.com>
2016-08-18 01:26:03 +0800

    边看权威指南边码代码
    我就不信过不了这个 瓶颈

---
+++ js/Task_seventeen.js
@@ -0,0 +1,21 @@
+// JavaScript Document
+/**
+ * Created by hansneil on 21/3/16.
+ */
+/* 数据格式演示
+ var aqiSourceData = {
+ "北京": {
+ "2016-01-01": 10,
+ "2016-01-02": 10,
+ "2016-01-03": 10,
+ "2016-01-04": 10
+ }
+ };
+ */
+
+// 以下两个函数用于随机模拟生成测试数据
+function getDateStr(dat){
+	var y = dat.getFullYear();
+	var m = dat.getMonth() + 1;
+	
+	}
